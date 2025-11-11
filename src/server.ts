7a9b8e19ca1a6a2bf26599a4e8beacfb4cd60176
diogonoguiera27 <diogoalvesnogueira3@gmail.com>
2025-11-11 15:26:45 -0300

    criando meus endpoints

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,32 @@ import fastify from "fastify";
 
 const server = fastify({ logger: true });
 
+const teams = [
+    { id: 1, name: "ferrari", base:"working united kingdom" },
+    { id: 2, name: "mercedes", base:"brackley united kingdom" },
+    { id: 3, name: "red bull racing", base:"milton keynes united kingdom" },
+
+]
+
+const drivers = [
+    { id:1 , name: "Max Verstappen", team: "Red Bull Racing" },
+    { id:2 , name: "Lewis Hamilton", team: "Mercedes" },
+    { id:3 , name: "Charles Leclerc", team: "Ferrari" },
+]
+
 server.get("/teams", async (request, response)=>{
     response.type("application/json").code(200);
     return [
-        { id: 1, name: "ferrari" },
+        {teams},
     ];
 });
+
+server.get("/drivers", async (request, response)=>{
+    response.type("application/json").code(200);
+    return [
+        { drivers },
+    ]
+})
 
 server.listen({ port: 3333}, ()=>{
     console.log("Server init");
